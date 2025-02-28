4dd16dc3c3bfc1ad8070452be87ac937f4117bc7
jah1044 <jhudak2004@gmail.com>
2025-02-28 09:48:20 -0500

    Add files via upload

---
+++ converter.py
@@ -0,0 +1,7 @@
+# converter.py
+
+def feet_to_meters(feet):
+    return round(feet * 0.3048, 2)
+
+def meters_to_feet(meters):
+    return round(meters / 0.3048, 2)
---
+++ sales_tax.py
@@ -0,0 +1,9 @@
+#sales_tax.py
+
+SALES_TAX_RATE = 0.06  
+
+def calculate_sales_tax(total):
+    return round(total * SALES_TAX_RATE, 2)
+
+def calculate_total_after_tax(total):
+    return round(total + calculate_sales_tax(total), 2)
